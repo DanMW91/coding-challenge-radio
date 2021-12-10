3cef93ef4803bf753420d59cda08c2df8814e96c
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-12-10 15:11:11 +0000

    finished front end challenge

--- src/components/station-item.js
+++ src/components/station-item.js
@@ -1,35 +1,64 @@
 import {useState} from 'react'
 import styled from 'styled-components';
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import radioImage from "./../assets/spaceman.jpg";
+import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const StyledStationItem = styled.div`
   display:flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  height: 50px;
   border-bottom: 1px solid grey;
   color: rgb(113,120,136);
   font-weight: bold;
 
 `
 
+const StationInfo = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  justify-content: space-between;
+`
+
+const StationDetail = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px 0px;
+`
+
+const RadioImage = styled.img`
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  border: 2px solid rgb(113,120,136);;
+`
+
 const StationItem = (props) => {
   const [selected, setSelected] = useState(false)
 
   return (
-    <>
       <StyledStationItem onClick={()=> {
         props.onSelectStation(props.name)
         setSelected(true)}
         }>
         {selected && props.activeStation === props.name &&
-        <FontAwesomeIcon icon={faPlusCircle}/>}
-        <div>{props.name}</div>
-        <div>{props.station}</div>
+        <StationDetail>
+          <FontAwesomeIcon icon={faMinusCircle}/>
+            <RadioImage src={radioImage}/>
+          <FontAwesomeIcon icon={faPlusCircle}/>
+        </StationDetail>
+        }
+        <StationInfo>
+          <div>{props.name}</div>
+          <div>{props.station}</div>
+        </StationInfo>
       </StyledStationItem>
-    </>
   )
 }
 
